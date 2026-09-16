b91a9b50c785e20e15362ad82f4ec4ea1224949c
KareemPermus <kareem.intern@permus.io>
2026-09-16 09:59:27 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,2 @@
-def total_calories(recipe):
-    total = 0
-    for item in recipe["ingredients"]:
-        total += item["calories"]
-    return total
+query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'"
+cursor.execute(query)
